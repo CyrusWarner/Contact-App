1a809a89167c54b26be1c63eaaf9f73462132aae
CyrusWarner <cyruswarner27@gmail.com>
2021-07-15 14:08:11 -0500

    Added contacts to local storage and display contacts is shown even on refresh.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,15 +1,26 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from './Header/header';
 import AddContact from './AddContact/addContact';
 import ContactList from './ContactList/contactList';
 
 function App() {
+    const LOCAL_STORAGE_KEY = "contacts";
     const [contacts, setContacts] = useState([]);
 
     const addContactHandler = (contact) => {
         console.log(contact);
         setContacts([...contacts, contact])
-    }
+    };
+
+    useEffect(() => {
+        const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+        if(retrieveContacts) setContacts(retrieveContacts);
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+    }, [contacts]);
+
     return (
         <div className="ui container">
             <Header />
